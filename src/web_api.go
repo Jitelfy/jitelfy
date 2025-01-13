3838629c82ba4d098a7fb5b67158775a9a83f0ac
alexeidoell <alexeidoell@gmail.com>
2025-01-12 21:07:44 -0600

    working rest api

--- src/web_api.go
+++ src/web_api.go
@@ -1,8 +1,39 @@
+package main
+
 import (
-  "net/http"
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
-func main() {
+type test struct {
+	Id       string `json:"id" bson:"id"`
+	Username string `json:"username" bson:"username"`
+}
 
+var testdb = []test{
+	{Id: "0", Username: "alexei"},
+	{Id: "1", Username: "k z 3"},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/testdb", getTest)
+	router.POST("/testdb", postTest)
+
+	router.Run("localhost:8080")
+
+}
+
+func getTest(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, testdb)
+}
+
+func postTest(c *gin.Context) {
+	var newUser test
+
+	if err := c.BindJSON(&newUser); err != nil {
+		return
+	}
+	testdb = append(testdb, newUser)
+	c.IndentedJSON(http.StatusOK, newUser)
 }
