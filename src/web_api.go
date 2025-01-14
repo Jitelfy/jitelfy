a764838644cfc36106f1b82337d67aa4cedcb597
Alexei Doell <alexeidoell@gmail.com>
2025-01-14 16:49:24 -0600

    have working logger

--- src/web_api.go
+++ src/web_api.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -14,20 +16,43 @@ type test struct {
 var testdb = []test{
 	{Id: "0", Username: "alexei"},
 	{Id: "1", Username: "k z 3"},
+	{Id: "2", Username: "fuck you abdullah"},
 }
+
+const (
+	colorReset = "\033[0m"
+	colorRed = "\033[31m"
+	colorGreen = "\033[32m"
+)
 
 func main() {
 	router := echo.New()
 	router.Debug = true
-	router.Use(middleware.Logger())
 	router.GET("/testdb", getTest)
 
+	var logger = middleware.RequestLoggerConfig{
+		LogStatus: true,
+		LogURI: true,
+		LogMethod: true,
+		LogLatency: true,
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			fmt.Printf("%v %v | ", v.Method, v.URI)
+			color := colorGreen
+			if v.Status != 200 {
+				color = colorRed
+			}
+			fmt.Printf("%v%v%v | %v\n", color, v.Status, colorReset, v.Latency)
+			return nil
+		},
+	}
+
+	router.Use(middleware.RequestLoggerWithConfig(logger))
+
 	router.Logger.Debug(router.Start("localhost:8080"))
-
-
 }
 
 func getTest(c echo.Context) error {
 	return c.JSONPretty(http.StatusOK, testdb, "    ")
 }
+
 
