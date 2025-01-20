05a74e4977764c601f646f8dfbc6bcaf0848594e
kevinzhang3 <kzhang366@gmail.com>
2025-01-20 15:52:57 -0600

    changed panics to print error, then return from main()

--- backend/src/web_api.go
+++ backend/src/web_api.go
@@ -45,17 +45,20 @@ func main() {
 	// create a client, connect to server
 	client, err := mongo.Connect(context.TODO(), opts)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
 	defer func() {
 		if err = client.Disconnect(context.TODO()); err != nil {
-			panic(err)
+			fmt.Println(err)
+			return
 		}
 	}()
 	// Send a ping to confirm a successful connection
 	if err := client.Database("admin").RunCommand(context.TODO(),
 		bson.D{{"ping", 1}}).Err(); err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
 	fmt.Println("Jitelfy successfully connected to Cluster Server.")
 
