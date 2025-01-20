b98054585b83fc62bfe48dbde20e0a0ec45f850f
booler <mangoabdullah@gmail.com>
2025-01-20 15:52:39 -0600

    Made company logo centered

--- jitelfy/src/App.tsx
+++ jitelfy/src/App.tsx
@@ -1,10 +1,10 @@
-
-function App() { 
+function App() {
   return (
-    <>
-      Jitelfy
-    </>
-  )
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-6xl font-bold">Jitelfy</h1>
+      <h2 className="text-sm mt-2">We're cooked</h2>
+    </div>
+  );
 }
 
-export default App
+export default App;
