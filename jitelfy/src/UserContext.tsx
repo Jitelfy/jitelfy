6c7321ed699246d804cedeaf1a9b0d804d31e386
Abdullah <mangoabdullah@gmail.com>
2025-02-22 04:05:32 -0600

    Context for the logged in user that each component can use

---
+++ jitelfy/src/UserContext.tsx
@@ -0,0 +1,21 @@
+import React, { createContext, useState, ReactNode } from "react";
+import { User } from "./App";
+
+interface UserContextType {
+  user: User | null;
+  setUser: (user: User | null) => void;
+}
+
+export const UserContext = createContext<UserContextType>({
+  user: null,
+  setUser: () => {},
+});
+
+export const UserProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
