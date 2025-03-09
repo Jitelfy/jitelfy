4abc4802978339d8f6445c4890bb39fa6253a994
job346 <job346@usask.ca>
2025-03-08 22:25:02 -0600

    Profile page redux

--- jitelfy/src/pages/ProfilePage.tsx
+++ jitelfy/src/pages/ProfilePage.tsx
@@ -27,19 +27,58 @@ const ProfilePage = () => {
       {Quicklinks(user)}
   
         {/* Main Content - Middle */}
-        <div className="flex-1 bg-background-main p-6 overflow-auto">
-          <div className="bg-background-secondary p-6 rounded-lg flex flex-col items-center">
-            <div className="relative w-full">
-              <div className="w-full h-48 bg-green-500 flex items-center justify-center">
-                <h1 className="text-4xl text-text-main">WE'RE COOKED</h1>
-              </div>
-              <img src={IconArray[parseInt(user?.icon, 10)]} className="absolute top-32 left-1/2 transform -translate-x-1/2 w-32 h-32 bg-background-tertiary rounded-full border-4 border-background-main"></img>
+        <div className="flex-1 flex-col bg-background-main p-6 overflow-auto">
+
+            {/* Container for all user data */}
+            <div className="flex flex-col items-center bg-background-secondary p-4 rounded-md">
+                {/* Banner */}
+                <div className="w-full h-44 rounded-md bg-accent-blue-light flex items-center justify-center">
+                </div>
+
+                {/* Icon, display & username, profile song */}
+                <div className="flex flex-row w-full self-start items-center">
+                    {/*transform -translate-x-1/2*/}
+                    <img src={IconArray[user?.icon]} className="absolute transform translate-x-1/4 w-32 h-32 bg-background-tertiary rounded-full border-4 border-background-main"></img>
+
+                    {/* Display name & username */}
+                    <div className="flex-col ml-44 mt-4 justify-start ml-3">
+                        <h2 className="text-xl text-text-main">{user?.displayname || 'user cannot be loaded'}</h2>
+                        <p className="text-md text-text-secondary">@{user?.username || 'username'}</p>
+                    </div>
+
+                    {/* DEBUG: Profile song */}
+                    {/*<div className="flex-col justify-start">
+                        <iframe
+                            src="https://open.spotify.com/embed/track/0gzy4HT0Ifnxm7T2U0XzdM?si=12a67ae2d83744e4"
+                            className="text-text-main"
+                            title="Profile song"
+                        ></iframe>
+                    </div>*/}
+                </div>
+
+                {/* Bio */}
+                <p className="text-text-main self-start ml-10 mr-10 mt-10 overflow-auto text-wrap">This is a biography. You should probably put some meaningful words here. I am typing a lot to see if this text will wrap around correctly.</p>
+
+                {/* Followers & following */}
+                <div className="flex flex-row w-full my-5 justify-evenly">
+                    <p className="text-text-main"><b>0</b> Followers</p>
+                    <p className="text-text-main"><b>0</b> Following</p>
+                </div>
+
+                <hr className="border-1 w-full self-start border-background-tertiary"></hr>
+
+                {/* SVG for showing profile stats */}
+                <svg
+                    className="self-end mt-5"
+                    fill="white" width="25px" height="25px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                    <g stroke="none" stroke-width="1" fill-rule="evenodd">
+                        <g id="ic_fluent_poll_24_regular" fill-rule="nonzero">
+                            <path d="M11.7518706,1.99956021 C13.2716867,1.99956021 14.5037411,3.23161462 14.5037411,4.75143076 L14.5037411,19.2499651 C14.5037411,20.7697812 13.2716867,22.0018356 11.7518706,22.0018356 C10.2320544,22.0018356 9,20.7697812 9,19.2499651 L9,4.75143076 C9,3.23161462 10.2320544,1.99956021 11.7518706,1.99956021 Z M18.7518706,6.99956021 C20.2716867,6.99956021 21.5037411,8.23161462 21.5037411,9.75143076 L21.5037411,19.2499651 C21.5037411,20.7697812 20.2716867,22.0018356 18.7518706,22.0018356 C17.2320544,22.0018356 16,20.7697812 16,19.2499651 L16,9.75143076 C16,8.23161462 17.2320544,6.99956021 18.7518706,6.99956021 Z M4.75187055,11.9995602 C6.27168669,11.9995602 7.5037411,13.2316146 7.5037411,14.7514308 L7.5037411,19.2499651 C7.5037411,20.7697812 6.27168669,22.0018356 4.75187055,22.0018356 C3.23205441,22.0018356 2,20.7697812 2,19.2499651 L2,14.7514308 C2,13.2316146 3.23205441,11.9995602 4.75187055,11.9995602 Z M11.7518706,3.49956021 C11.0604815,3.49956021 10.5,4.06004175 10.5,4.75143076 L10.5,19.2499651 C10.5,19.9413541 11.0604815,20.5018356 11.7518706,20.5018356 C12.4432596,20.5018356 13.0037411,19.9413541 13.0037411,19.2499651 L13.0037411,4.75143076 C13.0037411,4.06004175 12.4432596,3.49956021 11.7518706,3.49956021 Z M18.7518706,8.49956021 C18.0604815,8.49956021 17.5,9.06004175 17.5,9.75143076 L17.5,19.2499651 C17.5,19.9413541 18.0604815,20.5018356 18.7518706,20.5018356 C19.4432596,20.5018356 20.0037411,19.9413541 20.0037411,19.2499651 L20.0037411,9.75143076 C20.0037411,9.06004175 19.4432596,8.49956021 18.7518706,8.49956021 Z M4.75187055,13.4995602 C4.06048154,13.4995602 3.5,14.0600417 3.5,14.7514308 L3.5,19.2499651 C3.5,19.9413541 4.06048154,20.5018356 4.75187055,20.5018356 C5.44325957,20.5018356 6.0037411,19.9413541 6.0037411,19.2499651 L6.0037411,14.7514308 C6.0037411,14.0600417 5.44325957,13.4995602 4.75187055,13.4995602 Z">
+                            </path>
+                        </g>
+                    </g>
+                </svg>
             </div>
-            <div className="text-center mt-16">
-              <h2 className="text-2xl text-text-main">{user?.displayname || 'user cannot be loaded'}</h2>
-              <p className="text-text-secondary">@{user?.username || 'username'}</p>
-            </div>
-          </div>
         </div>
   
         {/* Sidebar - Right */}
