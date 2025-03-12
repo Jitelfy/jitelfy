7b6339da5860350746cea116c40db48dcb46a906
job346 <job346@usask.ca>
2025-03-11 23:26:20 -0600

    Settings banner bug fix Settings bio fix Notification alignment fix + ordering fix

--- jitelfy/src/pages/ProfilePage.tsx
+++ jitelfy/src/pages/ProfilePage.tsx
@@ -113,7 +113,7 @@ const ProfilePage = () => {
             {Quicklinks(user)}
 
             {/* Main Content - Middle */}
-            <div className="flex-1 flex-col bg-background-main px-10 p-6 overflow-auto">
+            <div className="flex-1 flex-col bg-background-main px-10 p-6 overflow-auto hide-scrollbar">
 
                 {/* Container for all user data */}
                 <div className="flex flex-col items-center bg-background-secondary p-4 rounded-md mb-8">
