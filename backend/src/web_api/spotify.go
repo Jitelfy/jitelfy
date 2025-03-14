2af0eea1139a075273e53b33c6702c774518eb02
kevinzhang3 <kzhang366@gmail.com>
2025-03-14 15:03:05 -0600

    callback handler

--- backend/src/web_api/spotify.go
+++ backend/src/web_api/spotify.go
@@ -1,10 +1,14 @@
 package web_api
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/labstack/echo/v4"
+	"io"
 	"net/http"
 	"net/url"
+	"strings"
+	"time"
 )
 
 const (
@@ -33,6 +37,59 @@ func SpotifyCallbackHandler(c echo.Context) error {
 		return c.String(http.StatusBadRequest, "No code provided")
 	}
 
-	// token exchange data
+	// token exchange req
 	data := url.Values{}
+	data.Set("grant_type", "authorization_code")
+	data.Set("code", code)
+	data.Set("redirect_uri", redirectURI)
+
+	req, err := http.NewRequest("POST", tokenURL, strings.NewReader(data.Encode()))
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.SetBasicAuth(clientID, clientSecret)
+
+	// response
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+	// ngl the ide told me to wrap this
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			return
+		}
+	}(resp.Body)
+
+	var tokenResp struct {
+		AccessToken  string `json:"access_token"`
+		TokenType    string `json:"token_type"`
+		ExpiresIn    int    `json:"expires_in"`
+		RefreshToken string `json:"refresh_token"`
+		Scope        string `json:"scope"`
+	}
+	if err := json.NewDecoder(resp.Body).Decode(&tokenResp); err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+	spotifyTokenCookie := &http.Cookie{
+		Name:     "spotify_access_token",
+		Value:    tokenResp.AccessToken,
+		Expires:  time.Now().Add(time.Hour * 72),
+		Path:     "/",
+		HttpOnly: true,
+		Secure:   true,
+	}
+	c.SetCookie(spotifyTokenCookie)
+	spotifyRefreshCookie := &http.Cookie{
+		Name:     "spotify_refresh_token",
+		Value:    tokenResp.RefreshToken,
+		Expires:  time.Now().Add(time.Hour * 72),
+		Path:     "/",
+		HttpOnly: true,
+		Secure:   true,
+	}
+	c.SetCookie(spotifyRefreshCookie)
+	return c.JSON(http.StatusOK, tokenResp)
 }
