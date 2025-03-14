5c658fa04a340de0f902dd40724057198a64a5bf
Alexei Doell <alexeidoell@gmail.com>
2025-03-14 09:02:14 -0600

    better validation on makeuser

--- backend/src/web_api/user.go
+++ backend/src/web_api/user.go
@@ -82,6 +82,32 @@ func MakeUser(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, "invalid json")
 	}
 
+	valid_user := make(chan int)
+	hash_complete := make(chan int)
+
+	go func() {
+		filter := bson.D{{Key: "username", Value: req.Username}}
+		result := UserColl.FindOne(context.TODO(), filter)
+		if result.Err() != mongo.ErrNoDocuments {
+			valid_user <- -1
+		} else {
+			valid_user <- 0
+		}
+	}()
+
+	var encryptedPass string
+	var err error
+
+	go func() {
+		encryptedPass, err = HashPassword(req.Password)
+		if err != nil {
+			hash_complete <- -1
+		} else {
+			hash_complete <- 0
+		}
+	}()
+
+
 	if len(req.DisplayName) > 40 {
 		return c.JSON(http.StatusBadRequest, "Display name must be under 41 characters.")
 	}
@@ -100,9 +126,12 @@ func MakeUser(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, "Password must be at least 8 characters.")
 	}
 
-	encryptedPass, err := HashPassword(req.Password)
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, "Password hashing failed.")
+	if <-valid_user == -1 {
+		return c.JSON(http.StatusForbidden, "User with this username already exists.")
+	}
+
+	if <-hash_complete == -1 { //  we should not tell them the reason it failed emily
+		return c.JSON(http.StatusInternalServerError, "Failed to create account.") 
 	}
 
 	user := User{
@@ -132,7 +161,6 @@ func MakeUser(c echo.Context) error {
 		UserId: user.Id,
 		PostIds: []primitive.ObjectID{},
 	}
-
 	_, err = UserColl.InsertOne(context.TODO(), user)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, "Failed to create user.")
