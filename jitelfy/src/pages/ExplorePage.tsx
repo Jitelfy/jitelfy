d631c82239d138cf529fb17c6e61571c306a964e
job346 <job346@usask.ca>
2025-03-16 12:21:58 -0600

    Explore page can scroll in margins

--- jitelfy/src/pages/ExplorePage.tsx
+++ jitelfy/src/pages/ExplorePage.tsx
@@ -120,7 +120,7 @@ const ExplorePage = () => {
             {Quicklinks(user)}
 
             {/* Main Content - Middle */}
-            <div className="flex-1 flex-col px-20 relative grid grid-auto-flow auto-rows-auto">
+            <div className="flex-1 flex-col px-20 relative overflow-y-auto hide-scrollbar">
                 <div className="fixed z-20 bg-background-main opacity-95 w-full">
                     <h1 className="text-white text-2xl top-0 my-6">Explore</h1>
                 </div>
