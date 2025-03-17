dcf703de0b1059f22de5714ef9a78475e892eddd
kevinzhang3 <kzhang366@gmail.com>
2025-03-17 11:30:42 -0600

    create playlis

--- backend/src/web_api/spotify_api.go
+++ backend/src/web_api/spotify_api.go
@@ -1,7 +1,57 @@
 package web_api
 
-import "github.com/labstack/echo/v4"
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"strings"
+)
 
-func ThreadPlaylist(c echo.Context) error {
+func trackURLToURI(url string) string {
+	parts := strings.Split(url, "/track/")
+	if len(parts) < 2 {
+		return ""
+	}
+	id := strings.Split(parts[1], "?")[0]
+	return "spotify:track:" + id
+}
 
+func createPlaylist(accessToken string, playlistName string, playlistDescription string, isPublic bool) (map[string]interface{}, error) {
+	body := map[string]interface{}{
+		"name":        playlistName,
+		"description": playlistDescription,
+		"public":      isPublic,
+	}
+
+	bodyBytes, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", "https://api.spotify.com/v1/me/playlists", bytes.NewBuffer(bodyBytes))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+accessToken)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			return
+		}
+	}(resp.Body)
+
+	var result map[string]interface{}
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return nil, err
+	}
+
+	return result, nil
 }
