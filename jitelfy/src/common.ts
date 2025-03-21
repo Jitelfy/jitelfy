4822767bd1c2a8fe84136aa23f6fdebd5a4dcf4f
job346 <job346@usask.ca>
2025-03-21 11:32:06 -0600

    Easier date formatting with library lol

--- jitelfy/src/common.ts
+++ jitelfy/src/common.ts
@@ -2,22 +2,13 @@
     For commonly used helper-functions across files.
  */
 
+import { format, formatDistance, subDays } from "date-fns";
+
 /**
  * Formats a given date to a nicer looking string.
  * @param date the date to format
  */
 export const formatDate = (date: Date) => {
-    let formatDate = date.toLocaleString();
-    let dateArr = date.toDateString().split(' ');
-
-    let newDate =  dateArr[1] + ' ' + dateArr[2] + ', ' + dateArr[3];
-
     /* Make time prettier */
-    if (formatDate.slice(-4) === "p.m.") {
-        newDate = newDate.concat(" " + formatDate.slice(-13, -8) + " PM");
-    } else {
-        newDate = newDate.concat(" " + formatDate.slice(-13, -8) + " AM");
-    }
-
-    return newDate;
+    return format(date, "dd MMM, yyyy HH:mm b");
 }
