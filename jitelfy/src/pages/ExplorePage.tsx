6f5a482dda0f0be8edb2b018dd83fd8c41fcae52
job346 <job346@usask.ca>
2025-03-21 12:28:57 -0600

    Search bar fix

--- jitelfy/src/pages/ExplorePage.tsx
+++ jitelfy/src/pages/ExplorePage.tsx
@@ -126,6 +126,8 @@ const ExplorePage = () => {
                 </div>
 
                 <div className="flex-1 bg-background-main relative overflow-auto hide-scrollbar pt-16">
+
+                    {/* Search bar */}
                     <div className="flex flex-row w-full items-center gap-4 bg-background-secondary fill-text-main rounded-t-md mt-4 px-4 pt-4 pb-6">
                             <svg width="25px" height="25px" viewBox="0 0 24 24">
                             <path fillRule="evenodd" clipRule="evenodd" d="M11 5C7.68629 5 5 7.68629 5 11C5 14.3137 7.68629 17 11 17C14.3137 17 17 14.3137 17 11C17 7.68629 14.3137 5 11 5ZM3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11C19 12.8487 18.3729 14.551 17.3199 15.9056L20.7071 19.2929C21.0976 19.6834 21.0976 20.3166 20.7071 20.7071C20.3166 21.0976 19.6834 21.0976 19.2929 20.7071L15.9056 17.3199C14.551 18.3729 12.8487 19 11 19C6.58172 19 3 15.4183 3 11Z"/>
@@ -141,7 +143,7 @@ const ExplorePage = () => {
                                         setSearchInput(userSearchInput);
                                     }
                                 }}
-                                className="flex-1 px-4 py-2 bg-background-tertiary text-text-main rounded-md focus:outline-none focus:ring-2 focus:ring-accent-blue"/>
+                                className="flex-1 px-4 py-2 bg-background-main border border-background-tertiary text-text-main rounded-md focus:outline-none focus:ring-2 focus:ring-accent-blue"/>
 
                             <button onClick={() => {
                                 setSearchInput(userSearchInput);
